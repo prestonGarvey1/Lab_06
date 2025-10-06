af3fc82704da6f6457ddc2d2fe2dfc89e7b30faf
prestonGarvy1 <thebozzkid2010@gmail.com>
2025-10-05 20:10:31 -0400

    initial

---
+++ src/CtoFConverter.java
@@ -0,0 +1,30 @@
+import java.nio.file.attribute.UserPrincipal;
+import java.util.Scanner;
+public class CtoFConverter {
+    public static void main(String[] args) {
+        // Create scanner object
+        Scanner scan = new Scanner(System.in);
+
+        // Values
+
+        double fahrenheit = 0.0;
+        boolean validInput;
+
+        do {
+            System.out.print("Enter temperature in Fahrenheit to be converted: ");
+            String input = scan.nextLine();
+            try {
+                fahrenheit = Double.parseDouble(input);
+                validInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a numeric value.");
+                validInput = false;
+            }
+        } while (!validInput);
+
+        double celsius = (5*(fahrenheit - 32.0) / 9.0);
+        System.out.printf("Temperature in fahrenheit: %.2f°F%n", fahrenheit);
+        System.out.printf("Temperature in celsius: %.2f°F%n", celsius);
+
+    }
+}
